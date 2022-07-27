840556ad02de43d245b9b1fa2335c3ab2f77eb9a
Denis Snezhko <mikle4421@mail.ru>
2022-07-27 23:20:05 +0300

    Initial commit

---
+++ 10-HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10-HW
+//
+//  Created by Denis Snezhko on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
